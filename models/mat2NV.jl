2d795cf0aa172a2e5378d003ba8b7b950b1a6ec4
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-06-01 15:48:37 -0300

    add docstring

--- models/mat2NV.jl
+++ models/mat2NV.jl
@@ -5,6 +5,22 @@ _vec(A::AbstractMatrix) = vec(A)
 _vec(A::Number) = [A]
 _vec(A::AbstractVector) = A
 
+
+"""
+    readnn(file, key="controller")
+
+Return the list of vertices of a polyhedron in constraint representation.
+
+### Input
+
+- `file` -- string indicating the location of the file containing the NN
+- `key` -- (optional, default: `"controller"`) key used to search the dictionary
+
+### Output
+
+A Neural Network using the internal format of NeuralVerification.jl
+
+"""
 function readnn(file, key="controller")
     vars = matread(file)
     !haskey(vars, key) && throw(ArgumentError("didn't find key $key"))
