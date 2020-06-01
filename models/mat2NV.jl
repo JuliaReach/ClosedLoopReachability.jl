56aa68f875a198bf068db5e1ac0d7e371ec6dc0c
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-06-01 13:28:45 -0300

    change notebook for script

---
+++ models/mat2NV.jl
@@ -0,0 +1,30 @@
+using NeuralVerification, MAT
+using NeuralVerification: Layer, Network, ReLU, Id
+
+_vec(A::AbstractMatrix) = vec(A)
+_vec(A::Number) = [A]
+_vec(A::AbstractVector) = A
+
+function readnn(file)
+    vars = matread(file)
+    dic = vars["controller"]
+
+    nLayers = Int(dic["number_of_layers"])
+    layers = Vector{Layer}(undef, nLayers)
+    aF = dic["activation_fcns"]
+
+    for n = 1:nLayers
+        W = dic["W"][n]
+        b = dic["b"][n]
+        if(aF[n] == "relu")
+            act = ReLU()
+        elseif(aF[n] == "linear")
+            act = Id()
+        else
+            error("error, aF = $(aF[n]), nLayer = $n")
+        end
+        layers[n] = Layer(W, _vec(b), act)
+    end
+
+    return Network(layers)
+ends
