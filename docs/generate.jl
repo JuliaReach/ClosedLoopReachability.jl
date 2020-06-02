5bf13f79730a2c56eb58728ee097cc06f8ded777
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-06-02 15:42:24 -0300

    disable credits for literate

--- docs/generate.jl
+++ docs/generate.jl
@@ -17,7 +17,7 @@ for model in MODELS
             script = Literate.script(input, GENERATEDDIR)
             code = strip(read(script, String))
             mdpost(str) = replace(str, "@__CODE__" => code)
-            Literate.markdown(input, GENERATEDDIR, postprocess = mdpost)
+            Literate.markdown(input, GENERATEDDIR, postprocess = mdpost, credit = false)
             Literate.notebook(input, GENERATEDDIR, execute = true)
         elseif any(endswith.(file, [".png", ".jpg", ".gif"]))
             cp(joinpath(model, file), joinpath(GENERATEDDIR, file); force=true)
