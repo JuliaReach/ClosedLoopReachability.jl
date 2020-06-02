53d2021e1fc8b52d300ff3ef38ee7d889610c60e
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-06-02 09:48:30 -0300

    change generate script

--- docs/generate.jl
+++ docs/generate.jl
@@ -1,20 +1,25 @@
 # generate examples
 import Literate
 
-EXAMPLEDIR = joinpath(@__DIR__, "..", "models")
+MODELS = [
+    joinpath(@__DIR__, "..", "models", "Sherlock-Benchmark-9"),
+    joinpath(@__DIR__, "..", "models", "Sherlock-Benchmark-10")
+]
 GENERATEDDIR = joinpath(@__DIR__, "src", "models")
 mkpath(GENERATEDDIR)
-for example in readdir(EXAMPLEDIR)
-    if endswith(example, ".jl")
-        input = abspath(joinpath(EXAMPLEDIR, example))
-        script = Literate.script(input, GENERATEDDIR)
-        code = strip(read(script, String))
-        mdpost(str) = replace(str, "@__CODE__" => code)
-        Literate.markdown(input, GENERATEDDIR, postprocess = mdpost)
-        Literate.notebook(input, GENERATEDDIR, execute = true)
-    elseif any(endswith.(example, [".png", ".jpg", ".gif"]))
-        cp(joinpath(EXAMPLEDIR, example), joinpath(GENERATEDDIR, example); force=true)
-    else
-        @warn "ignoring $example"
+for model in MODELS
+    for file in model
+        if endswith(file, ".jl")
+            input = abspath(joinpath(EXAMPLEDIR, model))
+            script = Literate.script(input, GENERATEDDIR)
+            code = strip(read(script, String))
+            mdpost(str) = replace(str, "@__CODE__" => code)
+            Literate.markdown(input, GENERATEDDIR, postprocess = mdpost)
+            Literate.notebook(input, GENERATEDDIR, execute = true)
+        elseif any(endswith.(file, [".png", ".jpg", ".gif"]))
+            cp(joinpath(model, file), joinpath(GENERATEDDIR, file); force=true)
+        else
+            @warn "ignoring $file in $model"
+        end
     end
 end
