ba7f1d686406d6eb7e8c3f977fb60a8429b84a78
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-06-11 08:45:03 -0300

    solve problems with latex

--- models/VertCAS/VertCAS.jl
+++ models/VertCAS/VertCAS.jl
@@ -18,19 +18,20 @@
 # assumed to maintain level flight. The system uses four variables to describe
 # the encounter with the intruder aircraft:
 #
-# 1)``h(ft)``: Intruder altitude relative to ownship``[−3000,3000]``
-# 2)``\dot h\0(ft/min)``: Ownship vertical climbrate``[−2500,2500]``
-# 3)``τ(s)``: Time to loss of horizontal separation``[0,40]``
-# 4)``s\_{adv}``: Previous advisory from VerticalCAS
+# 1) ``h(ft)``: Intruder altitude relative to ownship ``[−3000,3000]``
+# 2) ``\dot h_0 (ft/min)``: Ownship vertical climbrate ``[−2500,2500]``
+# 3) ``τ(s)``: Time to loss of horizontal separation ``[0,40]``
+# 4) ``s\{adv}``: Previous advisory from VerticalCAS
+#
 # The first two state variables describe the encounter geometry vertically.
 # The ``τ`` variable condenses the horizontal geometry into a single variable
 # by providing a countdown until the intruder will no longer be separated
 # horizontally, at whichpoint the ownship must be vertically separated to avoid
 # an NMAC. 
-# The ``s\_{adv}`` variable is categorical and can be any one of the nine
+# The ``s_{adv}`` variable is categorical and can be any one of the nine
 # possible advisories given by the system, and conditioning the next advisory
 # on the current advisory allows the system to maintain consistency when
-# alerting pilots. The nine possiblea dvisories are:
+# alerting pilots. The nine possible advisories are:
 #
 # 1) COC: Clear of Conflic
 # 2) DNC: Do Not Climb
@@ -44,10 +45,10 @@
 #
 # Each advisory instructs the pilot to accelerate until comply-ing with the
 # specified climb or descent rate, except for COC, which allows the pilot
-# freedom to choose any acceleration ``\ddot h\_0 ∈ [−g/8, g/8]``, where ``g``
+# freedom to choose any acceleration ``\ddot h_0 ∈ [−g/8, g/8]``, where ``g``
 # is the sea-level gravitational acceleration constant.
 # For advisories DNC, DND, DES1500, and CL1500, the pilot is assumed to
-# accelerate in the range ``|a| ∈ [g/4,g/3]`` with the sign of ``\ddot h\_0``
+# accelerate in the range ``|a| ∈ [g/4,g/3]`` with the sign of ``\ddot h_0``
 # determined by the specific advisory. If the pilot is already compliant with
 # the given advisory, then the pilot is assumed to continue at the current
 # climbrate. For advisories SDES1500, SCL1500, SDES2500, and SCL2500, the pilot
@@ -55,7 +56,7 @@
 # receiving the CL1500 advisory while descending at −500 ft/min is assumed to
 # begin accelerating upwards with some acceleration between ``g/4`` and ``g/3``
 # and then maintaining a constant climbrate upon reaching the 1500 ft/min
-# climbrate. New advisories ``s\_{adv}`` are given once each second ``(∆t= 1)``
+# climbrate. New advisories ``s_{adv}`` are given once each second ``(∆t= 1)``
 #
 
 using NeuralNetworkAnalysis
@@ -85,7 +86,8 @@ end
 
 # [1] [Julian, K. D., & Kochenderfer, M. J. (2019). A reachability method for
 # verifying dynamical systems with deep neural network controllers.
-# arXiv preprint arXiv:1903.00520.](https://arxiv.org/pdf/1903.00520.pdf).
+# arXiv preprint arXiv:1903.00520.](https://arxiv.org/pdf/1903.00520.pdf)
+#
 # [2] Akintunde, M. E., Botoeva, E., Kouvaros, P., & Lomuscio, A. (2020, May).
 # [Formal Verification of Neural Agents in Non-deterministic Environments.
 # In Proceedings of the 19th International Conference on Autonomous Agents and
