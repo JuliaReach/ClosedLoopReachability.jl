d61386a1587945cf92ea83a94943eaa6da4abf60
Christian Schilling <christian.schilling@ist.ac.at>
2021-02-22 12:18:25 +0100

    remove broken model from build

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ makedocs(
                           "Non-Linear Cart-Pole" => "models/Cart-Pole.md",
                           "Adaptive Cruise Controller" => "models/ACC.md",
 #                           "Airplane" => "models/Airplane.md",
-                          "Single Pendulum" => "models/SinglePendulum.md",
+#                           "Single Pendulum" => "models/SinglePendulum.md",
                           "Double Pendulum" => "models/DoublePendulum.md",
                           "Vertical CAS" => "models/VertCAS.md",
                          ],
