6816816932e81738efd929a58ff2745c45ce16e0
Marcelo Forets <mforets@gmail.com>
2021-05-17 15:31:56 -0300

    Update VertCAS.jl

--- models/VertCAS/VertCAS.jl
+++ models/VertCAS/VertCAS.jl
@@ -64,8 +64,8 @@
 # where $g$ represents the gravitational constant ``32.2 \ \text{ft/s}^2``.
 # If the new advisory is COC(1), then it can be any acceleration from the set ``{−g/8, 0, g/8}``.
 # For all remaining advisories, if the previous advisory coincides with the new one and the
-# current climb rate complies with the new advisory (e.g., ``\dot{h}0`` is non-positive for DNC and
-# ``\dot{h}0`` ≥ 1500 for CL1500) the acceleration `\ddot{h}0`` is ``0``
+# current climb rate complies with the new advisory (e.g., ``\dot{h}_0`` is non-positive for DNC and
+# ``\dot{h}_0 ≥ 1500`` for CL1500) the acceleration `\ddot{h}_0`` is ``0``.
 #
 # Given the current system state ``(h,\dot{h}_0,\tau,\text{adv})``, the new
 # advisory ``\text{adv}'`` and the acceleration ``\ddot{h}_0``, the new state
