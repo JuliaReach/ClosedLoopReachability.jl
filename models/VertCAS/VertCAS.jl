002fc393fd6255774055b80275e4a37af2a38636
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2021-05-20 16:09:06 -0300

    update model

--- models/VertCAS/VertCAS.jl
+++ models/VertCAS/VertCAS.jl
@@ -83,13 +83,132 @@
 # where ``\Delta\tau=1``.
 #
 
-using NeuralNetworkAnalysis
+g = 32.2 # gravitational constant
 
-@taylorize function VCAS!(dx, x, p, t)
-    tau, adv = x
-    dx[1] = x₁
-    dx[2] = x₂
-    dx[3] = x₃
+## accelerations (middle)
+const ACC_MIDDLE = Dict(:COC => 0.0, :DNC => -7g/24, :DND => 7g/24,
+                        :DES1500 => -7g/24, :CL1500 => 7g/24, :SDES1500 => -g/3,
+                        :SCL1500 => g/3, :SDES2500 => -g/3, :SCL2500 => g/3)
+
+## small (h, hdot0) model
+const Δτ = 1.0
+const A = [1  -Δτ; 0  1.]
+
+# We load the controllers in a dictionary with the keys beign the advisories.
+
+CONTROLLERS = Dict{Symbol, Any}()
+
+CTRL_IDX = [:COC, :DNC, :DND, :DES1500,
+            :CL1500, :SDES1500, :SCL1500,
+            :SDES2500, :SCL2500]
+
+path = read_nnet_mat(@modelpath("VertCAS");
+for i = 1:9
+    file = joinpath(path, "VertCAS_noResp_pra0$(i)_v9_20HU_200.nnet")
+    adv = CTRL_IDX[i]
+    CONTROLLERS[adv] = read_nnet(file)
+end;
+
+
+struct State{T, N}
+    state::T # state for (h, hdot0) variables
+    τ::N
+    adv::Symbol
+end
+
+
+ADVISORIES = Dict{Symbol, Any}()
+
+## for every advisory, return a function to check
+## whether the current climbrate complies with the advisory or not
+## if the advisory *does not* comply => it is changed according to the ACC dictionary
+## otherwise, the new acceleration is zero
+using Symbolics
+@variables x
+
+ADVISORIES[:COC] = EmptySet(1)
+ADVISORIES[:DNC] = HalfSpace(x <= 0)
+ADVISORIES[:DND] = HalfSpace(x >= 0)
+ADVISORIES[:DES1500] = HalfSpace(x <= -1500)
+ADVISORIES[:CL1500] = HalfSpace(x >= 1500)
+ADVISORIES[:SDES1500] = HalfSpace(x <= -1500)
+ADVISORIES[:SCL1500] = HalfSpace(x >= 1500)
+ADVISORIES[:SDES2500] = HalfSpace(x <= -2500)
+ADVISORIES[:SCL2500] = HalfSpace(x >= 2500)
+
+## this function receives X = [h, hdot0, τ, adv′] and the
+## *previous* advisory adv
+function get_acceleration(X::State, adv; ACC=ACC_MIDDLE)
+
+    ## obtain projection on hdot
+    hdot = _interval(X.state, 2)
+
+    ## transform units from ft/s to ft/min
+    hdot = 60 * hdot
+
+    ## new advisory
+    adv′ = X.adv
+
+    ## check whether the current state complies with the advisory
+    comply = hdot ⊆ ADVISORIES[adv′]
+
+    if adv == adv′ && comply
+        return 0.0
+    else
+        return ACC[adv′]
+    end
+end
+
+## scalar case; alg is ignored
+function forward_adv(X::Singleton, τ, adv; alg=nothing)
+    v = vcat(element(X), τ*1.0)
+    u = forward(CONTROLLERS[adv], v)
+    imax = argmax(u)
+    return CTRL_IDX[imax]
+end
+
+## set-based case
+function forward_adv(X::AbstractZonotope, τ, adv; alg=Ai2())
+    Y = cartesian_product(X, Singleton([τ*1.0]))
+
+    out = forward_network(alg, CONTROLLERS[adv], Y)
+
+    imax = argmax(high(out))
+    return CTRL_IDX[imax]
+end
+
+function VCAS!(out::Vector{State{T, N}}, KMAX; ACC=ACC_MIDDLE, alg_nn=Ai2()) where {T, N}
+
+    ## unpack initial state
+    X0 = first(out)
+    S = X0.state
+    τ = X0.τ
+    adv = X0.adv
+
+    ## get initial acceleration
+    hddot = ACC[adv]
+
+    for i in 1:KMAX
+        ## compute next state
+        b = [-hddot*Δτ^2 / 2, hddot * Δτ]
+        S′ = affine_map(A, S, b)
+        τ′ = τ - 1
+        adv′ = forward_adv(S′, τ′, adv, alg=alg_nn)
+
+        ## store new state
+        X′ = State(S′, τ′, adv′)
+        push!(out, X′)
+
+        ## get acceleration from network
+        ## this logic only works for ACC_MIDDLE
+        hddot = get_acceleration(X′, adv; ACC=ACC)
+
+        ## update current state
+        S = S′
+        τ = τ′
+        adv = adv′
+    end
+    return out
 end
 
 # ## Specifications
@@ -103,13 +222,74 @@ end
 
 # ## Results
 
-# define the initial-value problem
-##X₀ = Hyperrectangle(low=[...], high=[...])
+# ### Simulation
 
-##prob = @ivp(x' = VCAS!(x), dim: ?, x(0) ∈ X₀)
+function _default_state()
+    h0 = Interval(-133, -129)
+    hdot0 = [-19.5,-22.5, -25.5, -28.5]
+    x = sample(h0)[1]
+    y = hdot0[rand(1:4)]
+    τ0 = 25
+    adv0 = :COC
+    return State(Singleton([x, y]), τ0, adv0)
+end
 
-# solve it
-##sol = solve(prob, T=0.1);
+function simulate_VCAS(X0::State=_default_state(); KMAX=10)
+    out = [X0]
+    sizehint!(out, KMAX+1)
+
+    VCAS!(out, KMAX, ACC=ACC_MIDDLE)
+    return out
+end
+
+## project onto the h variable
+function _project(X::Vector{State{T, N}}) where {T<:Singleton, N}
+    return [Singleton([Xi.state.element[1], Xi.τ*1.0]) for Xi in X]
+end
+
+_interval(X::LazySet, i) = overapproximate(Projection(X, (i,)), Interval)
+
+function _project(X::Vector{State{T, N}}) where {T<:Zonotope, N}
+    Xint = [_interval(Xi.state, 1) × Singleton([Xi.τ*1.0]) for Xi in X]
+end
+
+ensemble = [simulate_VCAS() for _ in 1:10];
+
+out = _project.(ensemble)
+
+bad_states = HalfSpace([1.0, 0.0], 100.) ∩ HalfSpace([-1.0, 0.0], 100.)
+
+fig = plot(ylab="τ (time to reach horizontally)", xlab="h (vertical distance)")
+plot!(fig, bad_states, xlims=(-200, 200), ylims=(14, 26), alpha=0.3, c=:red)
+for o in out
+    plot!(fig, o, alpha=1.)
+end
+fig
+
+# ### Flowpipe resut for all choices of velocity
+
+h0 = Interval(-133, -129)
+hdot0 = [-19.5,-22.5, -25.5, -28.5]
+
+S0 = [convert(Zonotope, concretize(h0 × Singleton([hdot0[i]]))) for i in 1:4]
+
+τ0 = 25
+adv0 = :COC
+X0 = [State(S0i, τ0, adv0) for S0i in S0]
+
+result = [simulate_VCAS(X0i) for X0i in X0];
+
+out = _project.(result);
+
+bad_states = HalfSpace([1.0, 0.0], 100.) ∩ HalfSpace([-1.0, 0.0], 100.)
+
+fig = plot(ylab="τ (time to reach horizontally)", xlab="h (vertical distance)")
+plot!(fig, bad_states, xlims=(-200, 200), ylims=(14, 26), alpha=0.2, c=:red)
+[plot!(fig, o, lw=2.0, alpha=1., markershape=:none, seriestype=:shape, c=:blue) for o in out[1]]
+[plot!(fig, o, lw=2.0, alpha=1., markershape=:none, seriestype=:shape, c=:orange) for o in out[2]]
+[plot!(fig, o, lw=2.0, alpha=1., markershape=:none, seriestype=:shape, c=:green) for o in out[3]]
+[plot!(fig, o, lw=2.0, alpha=1., markershape=:none, seriestype=:shape, c=:cyan) for o in out[4]]
+fig
 
 # ## References
 
