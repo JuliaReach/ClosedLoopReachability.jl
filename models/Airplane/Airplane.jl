af711a7420a8f3760f31f281a7f8beef1976706c
schillic <git@christianschilling.net>
2021-06-02 13:19:53 +0200

    import instead of using

--- models/Airplane/Airplane.jl
+++ models/Airplane/Airplane.jl
@@ -173,7 +173,7 @@ alg_nn = Ai2();
 # @time sol = solve(prob, T=T, alg_nn=alg_nn, alg=alg);
 
 # We also compute some simulations:
-using DifferentialEquations
+import DifferentialEquations
 @time sim = simulate(prob, T=T; trajectories=10, include_vertices=true);
 
 # Finally we plot the results
