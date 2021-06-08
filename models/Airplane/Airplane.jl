5b2da69baa7f14d7df55bd3de3a86a9224c22f00
schillic <git@christianschilling.net>
2021-06-08 10:38:39 +0200

    refactor Airplane model

--- models/Airplane/Airplane.jl
+++ models/Airplane/Airplane.jl
@@ -11,10 +11,24 @@
 # \end{aligned}
 # ```
 #
-# where ``(x, y, z)`` is the position of the C.G., ``(u, v, w)`` are the
-# components of velocity in ``(x, y, z)`` directions, ``(p, q, r)`` are body
-# rotation rates, and ``(ϕ, θ, ψ)`` are the Euler angles.
+# where $(x, y, z)$ is the position of the center of gravity (C.G.),
+# $(u, v, w)$ are the components of velocity in $(x, y, z)$ directions,
+# $(p, q, r)$ are body rotation rates, and $(ϕ, θ, ψ)$ are the Euler angles.
 #
+# See also the file `airplane.png` for a visualization of the meaning of the
+# coordinates with respect to the airplane.
+
+module Airplane  #jl
+
+using NeuralNetworkAnalysis
+using NeuralNetworkAnalysis: SingleEntryVector
+
+# The following option determines whether the falsification settings should be
+# used or not. The falsification settings are sufficient to show that the safety
+# property is violated. Concretely we start from an initial point and use a
+# smaller time step.
+const falsification = true;
+
 # ## Model
 #
 # The equations of motion are reduced to:
@@ -30,12 +44,12 @@
 # \end{array} \right.
 # ```
 #
-# The mass of the airplane is denoted with m and ``I_x`` , ``I_y`` , ``I_z``
-# and ``I_{xz}`` are the moment of inertia with respect to the indicated axis.
-# The control parameters include three force components ``F_x`` , ``F_y`` and
-# ``F_z`` and three moment components ``M_x`` , ``M_y`` , ``M_z`` . Notice that
+# The mass of the airplane is denoted with m and $I_x$ , $I_y$ , $I_z$
+# and $I_{xz}$ are the moment of inertia with respect to the indicated axis.
+# The control parameters include three force components $F_x$ , $F_y$ and
+# $F_z$ and three moment components $M_x$ , $M_y$ , $M_z$ . Notice that
 # for simplicity we have assumed the aerodynamic forces are absorbed in the
-# ``F``’s. In addition to these six equations, we have six additional kinematic
+# $F$'s. In addition to these six equations, we have six additional kinematic
 # equations:
 #
 # ```math
@@ -79,39 +93,38 @@
 #
 # For the simplicity of control design, the parameters have been chosen to
 # have some nominal dimensionless values:
-# ``m = 1``, ``I_x = I_y = I_z = 1``, ``I_{xz} = 0`` and ``g = 1``.
-
-using NeuralNetworkAnalysis
-using NeuralNetworkAnalysis: SingleEntryVector
+# $m = 1$, $I_x = I_y = I_z = 1$, $I_{xz} = 0$ and $g = 1$.
 
 Tψ = ψ -> [cos(ψ)  -sin(ψ)  0;
            sin(ψ)   cos(ψ)  0;
-                0        0  1];
+                0        0  1]
 
 Tθ = θ -> [ cos(θ)  0  sin(θ);
                  0  1       0;
-           -sin(θ)  0  cos(θ)];
+           -sin(θ)  0  cos(θ)]
 
 Tϕ = ϕ -> [1       0        0;
            0  cos(ϕ)  -sin(ϕ);
-           0  sin(ϕ)   cos(ϕ)];
+           0  sin(ϕ)   cos(ϕ)]
 
 Rϕθ = (ϕ, θ) -> [1  tan(θ) * sin(ϕ)  tan(θ) * cos(ϕ);
                  0           cos(θ)          -sin(ϕ);
-                 0  sec(θ) * sin(ϕ)  sec(θ) * cos(ϕ)];
+                 0  sec(θ) * sin(ϕ)  sec(θ) * cos(ϕ)]
 
-# alternative matrix with only sin/cos but with postprocessing
-# Rϕθ_ = (ϕ, θ) -> [cos(θ)  sin(θ) * sin(ϕ)   sin(θ) * cos(ϕ);
-#                        0  cos(θ) * cos(ϕ)  -cos(θ) * sin(ϕ);
-#                        0           sin(ϕ)            cos(ϕ)];
+## alternative matrix with only sin/cos but with postprocessing
+## Rϕθ_ = (ϕ, θ) -> [cos(θ)  sin(θ) * sin(ϕ)   sin(θ) * cos(ϕ);
+##                        0  cos(θ) * cos(ϕ)  -cos(θ) * sin(ϕ);
+##                        0           sin(ϕ)            cos(ϕ)]
 
-# model constants
+## model constants
 const m = 1.0
 const g = 1.0
-const Ix = 1.0  # not used (terms are simplified)
-const Iy = 1.0  # not used (terms are simplified)
-const Iz = 1.0  # not used (terms are simplified)
-const Ixz = 0.0  # not used (terms are simplified)
+
+## unused constants (terms are simplified instead)
+const Ix = 1.0
+const Iy = 1.0
+const Iz = 1.0
+const Ixz = 0.0
 
 @taylorize function airplane!(dx, x, p, t)
     _x, y, z, u, v, w, ϕ, θ, ψ, r, _p, q, Fx, Fy, Fz, Mx, My, Mz = x
@@ -146,50 +159,93 @@ const Ixz = 0.0  # not used (terms are simplified)
     dx[18] = zero(Mz)
 end;
 
-controller = read_nnet(@modelpath("Airplane", "controller_airplane.nnet"));
-
 # ## Specifications
-#
-# ``x_2`` should be in ``[−0.5, 0.5]`` and ``x_7, x_8, x_9`` should be in ``[-1.0,1.0]``.
+
+controller = read_nnet(@modelpath("Airplane", "controller_airplane.nnet"))
 
 X₀ = Hyperrectangle(low=[0.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    high=[0.0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0]);
-U₀ = ZeroSet(6);
+                    high=[0.0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0])
+if falsification
+    ## choose a single point in the initial states (here: the top-most one)
+    X₀ = Singleton(high(X₀))
+end
+U₀ = ZeroSet(6)
 
-vars_idx = Dict(:state_vars=>1:12, :control_vars=>13:18);
-ivp = @ivp(x' = airplane!(x), dim: 18, x(0) ∈ X₀ × U₀);
+vars_idx = Dict(:state_vars=>1:12, :control_vars=>13:18)
+ivp = @ivp(x' = airplane!(x), dim: 18, x(0) ∈ X₀ × U₀)
 
-period = 0.1;  # control period
-T = 20 * period;  # time horizon
+period = 0.1  # control period
 
 prob = ControlledPlant(ivp, controller, vars_idx, period);
 
-safe_states = HPolyhedron([
-    HalfSpace(SingleEntryVector(2, 18, 1.0), 0.5),
-    HalfSpace(SingleEntryVector(2, 18, -1.0), 0.5),
-    HalfSpace(SingleEntryVector(7, 18, 1.0), 1.0),
-    HalfSpace(SingleEntryVector(7, 18, -1.0), 1.0),
-    HalfSpace(SingleEntryVector(8, 18, 1.0), 1.0),
-    HalfSpace(SingleEntryVector(8, 18, -1.0), 1.0),
-    HalfSpace(SingleEntryVector(9, 18, 1.0), 1.0),
-    HalfSpace(SingleEntryVector(9, 18, -1.0), 1.0)
-   ]);
-# TODO spec: x[2] ∈ [−0.5, 0.5] and [x[7], x[8], x[9]] ∈ [-1, 1]^3 for all t
+# Safety specification: $x_2$ should be in $[−0.5, 0.5]$ and $x_7, x_8, x_9$
+# should be in $[-1, 1]$.
+if falsification
+    k = 4  # falsification can run for a shorter time horizon
+else
+    k = 20
+end
+T = k * period  # time horizon
+
+safe_states = HPolyhedron([HalfSpace(SingleEntryVector(2, 18, 1.0), 0.5),
+                           HalfSpace(SingleEntryVector(2, 18, -1.0), 0.5),
+                           HalfSpace(SingleEntryVector(7, 18, 1.0), 1.0),
+                           HalfSpace(SingleEntryVector(7, 18, -1.0), 1.0),
+                           HalfSpace(SingleEntryVector(8, 18, 1.0), 1.0),
+                           HalfSpace(SingleEntryVector(8, 18, -1.0), 1.0),
+                           HalfSpace(SingleEntryVector(9, 18, 1.0), 1.0),
+                           HalfSpace(SingleEntryVector(9, 18, -1.0), 1.0)])
+
+## property for guaranteed violation
+predicate = X -> isdisjoint(overapproximate(X, Hyperrectangle), safe_states)
+predicate_sol = sol -> any(predicate(R) for F in sol for R in F);
 
 # ## Results
 
-alg = TMJets(abstol=1e-15, orderT=7, orderQ=1);
-alg_nn = Ai2();
-
-# @time sol = solve(prob, T=T, alg_nn=alg_nn, alg=alg);  # TODO uncomment once the analysis works
-
-# We also compute some simulations:
 import DifferentialEquations
-@time sim = simulate(prob, T=T; trajectories=50, include_vertices=true);
 
-# Finally we plot the results
+alg = TMJets(abstol=1e-10, orderT=7, orderQ=1)
+alg_nn = Ai2()
+
+function benchmark(; silent::Bool=false)
+    ## We solve the controlled system:
+    silent || println("flowpipe construction")
+    res_sol = @timed solve(prob, T=T, alg_nn=alg_nn, alg=alg)
+    sol = res_sol.value
+    silent || print_timed(res_sol)
+
+    ## Next we check the property for an overapproximated flowpipe:
+    silent || println("property checking")
+    solz = overapproximate(sol, Zonotope)
+    res_pred = @timed predicate_sol(solz)
+    silent || print_timed(res_pred)
+    if res_pred.value
+        silent || println("The property is violated.")
+    else
+        silent || println("The property may be satisfied.")
+    end
+
+    ## We also compute some simulations:
+    silent || println("simulation")
+    trajectories = falsification ? 1 : 50
+    res_sim = @timed simulate(prob, T=T, trajectories=trajectories,
+                              include_vertices=!falsification)
+    sim = res_sim.value
+    silent || print_timed(res_sim)
+
+    return solz, sim
+end
+
+benchmark(silent=true)  # warm-up
+sol, sim = benchmark();  # benchmark
+
+# Finally we plot the results:
+
 using Plots
 import DisplayAs
+
+## set more precise tolerance for plotting small sets correctly
+LazySets.set_ztol(Float64, 1e-9)
 
 function plot_helper(fig, vars)
     if vars[1] == 0
@@ -199,33 +255,42 @@ function plot_helper(fig, vars)
     else
         safe_states_projected = project(safe_states, vars)
     end
-    plot!(fig, safe_states_projected, color=:white, linecolor=:black, lw=5.0)
+    plot!(fig, safe_states_projected, color=:lightgreen, linecolor=:black, lw=5.0)
     if 0 ∉ vars
-        plot!(fig, project(X₀, vars), lab="X₀")
+        plot!(fig, project(initial_state(prob), vars), lab="X₀")
     end
-##    plot!(fig, sol, vars=vars, color=:orange, lab="");  # TODO uncomment once the analysis works
-    plot_simulation!(fig, sim; vars=vars, color=:red, lab="");
+    plot!(fig, sol, vars=vars, color=:orange, lab="")
+    plot_simulation!(fig, sim; vars=vars, color=:red, lab="")
     fig = DisplayAs.Text(DisplayAs.PNG(fig))
 end
 
-vars = (2, 7);
-fig = plot(xlab="x₂", ylab="x₇");
-xlims!(-1.8, 22.5)
-ylims!(-1.05, 1.05)
+vars = (2, 7)
+fig = plot(xlab="x₂", ylab="x₇", leg=:bottomleft)
 plot_helper(fig, vars)
+if falsification
+    xlims!(-0.01, 0.65)
+    ylims!(0.9, 1.01)
+else
+    xlims!(-1.8, 22.5)
+    ylims!(-1.05, 1.05)
+end
+## savefig("Airplane-x2-x7.pdf")
 
 #-
 
-vars = (8, 9);
-fig = plot(xlab="x₈", ylab="x₉", leg=:bottomleft);
-xlims!(-1.05, 1.2)
-ylims!(-1.05, 1.2)
+vars = (8, 9)
+fig = plot(xlab="x₈", ylab="x₉", leg=:bottom)
 plot_helper(fig, vars)
+if falsification
+    xlims!(0.999, 1.03)
+    ylims!(0.99, 1.001)
+else
+    xlims!(-1.05, 1.2)
+    ylims!(-1.05, 1.2)
+end
+## savefig("Airplane-x8-x9.pdf")
 
-#= TODO old code related to property checking
-solz = overapproximate(sol, Zonotope);
-@show ρ(sparsevec([2], [1.0], 18), solz)
-@show -ρ(sparsevec([2], [-1.0], 18), solz)
-[radius(solz.F.ext[:controls][i][1]) for i in 1:3]
-radius(overapproximate(solz.F.ext[:controls][3][1], Hyperrectangle))
-=#
+#-
+
+end  #jl
+nothing  #jl
