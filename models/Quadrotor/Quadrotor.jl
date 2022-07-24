5aeed5a8146b6a94a342bb536d24d3d545caa19b
schillic <git@christianschilling.net>
2022-07-24 11:24:01 +0200

    remove printing from new models

--- models/Quadrotor/Quadrotor.jl
+++ models/Quadrotor/Quadrotor.jl
@@ -216,8 +216,6 @@ res = @timed benchmark()  # benchmark
 sol = res.value
 println("total analysis time")
 print_timed(res);
-io = isdefined(Main, :io) ? Main.io : stdout
-print(io, "JuliaReach, Quadrotor, small X0, verified, $(res.time)\n")
 
 # We also compute some simulations:
 
