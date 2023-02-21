3cfba9cfdb59ac28bf10f6b5af3a6fef8dd76066
schillic <git@christianschilling.net>
2023-02-21 22:27:42 +0100

    add FileFormats module

--- src/DNN/DNN.jl
+++ src/DNN/DNN.jl
@@ -1,25 +1,9 @@
 module DNN
 
-using Requires, Reexport
-
-using ReachabilityBase.Require
-
-export read_nnet,
-       read_nnet_mat,
-       read_nnet_yaml,
-       read_nnet_sherlock, write_nnet_sherlock,
-       read_nnet_onnx,
-       read_nnet_polar
-
-include("init.jl")
+using Reexport
 
 include("Architecture/Architecture.jl")
 
-include("FileFormats/nnet.jl")
-include("FileFormats/MAT.jl")
-include("FileFormats/YAML.jl")
-include("FileFormats/Sherlock.jl")
-include("FileFormats/ONNX.jl")
-include("FileFormats/POLAR.jl")
+include("FileFormats/FileFormats.jl")
 
 end  # module
---
+++ src/DNN/FileFormats/FileFormats.jl
@@ -0,0 +1,23 @@
+@reexport module FileFormats
+
+using ..Architecture
+using Requires
+using ReachabilityBase.Require
+
+export read_nnet,
+       read_nnet_mat,
+       read_nnet_yaml,
+       read_nnet_sherlock, write_nnet_sherlock,
+       read_nnet_onnx,
+       read_nnet_polar
+
+include("init.jl")
+
+include("nnet.jl")
+include("MAT.jl")
+include("YAML.jl")
+include("Sherlock.jl")
+include("ONNX.jl")
+include("POLAR.jl")
+
+end  # module
