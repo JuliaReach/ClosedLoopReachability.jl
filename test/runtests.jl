481e05a8ab41b519ef2e2f583a5c51073336b6e9
schillic <git@christianschilling.net>
2023-05-05 07:26:47 +0200

    format test code

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,5 @@
 using Test, ClosedLoopReachability
 
-@testset "Toy model (black-box network)" begin include("black_box_toy_model.jl") end
+@testset "Toy model (black-box network)" begin
+    include("black_box_toy_model.jl")
+end
