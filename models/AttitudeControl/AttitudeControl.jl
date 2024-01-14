6dae644e41069eab65b63971f48c39dbd90e4b4f
schillic <git@christianschilling.net>
2024-01-14 23:36:58 +0100

    revise AttitudeControl model

--- models/AttitudeControl/AttitudeControl.jl
+++ models/AttitudeControl/AttitudeControl.jl
@@ -1,138 +1,169 @@
 # # Attitude Control
 #
 #md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/models/AttitudeControl.ipynb)
+#
+# The Attitude Control benchmark models a rigid-body system [^PPR].
 
 module AttitudeControl  #jl
 
 using ClosedLoopReachability
+import DifferentialEquations, Plots, DisplayAs
+using Plots: plot, plot!
 
 # ## Model
 
-@taylorize function AttitudeControl!(dx, x, p, t)
-    ω₁ = x[1]
-    ω₂ = x[2]
-    ω₃ = x[3]
-    ψ₁ = x[4]
-    ψ₂ = x[5]
-    ψ₃ = x[6]
-    u₀ = x[7]
-    u₁ = x[8]
-    u₂ = x[9]
+# There are 6 state variables: $(ω_1, ω_2, ω_3, ψ_1, ψ_2, ψ_3)$. The system
+# dynamics are given as follows:
+#
+# ```math
+# \begin{aligned}
+# \dot{ω}_1 &= 0.25 (u_0 + ω_2 ω_3) \\
+# \dot{ω}_2 &= 0.5 (u_1 - 3 ω_1 ω_3) \\
+# \dot{ω}_3 &= u_2 + 2 ω_1 ω_2) \\
+# \dot{ψ}_1 &= 0.5 (ω₂ (ξ - ψ₃) + ω₃ (ξ + ψ₂) + ω₁ (ξ + 1)) \\
+# \dot{ψ}_2 &= 0.5 (ω₁ (ξ + ψ₃) + ω₃ (ξ - ψ₁) + ω₂ (ξ + 1)) \\
+# \dot{ψ}_3 &= 0.5 (ω₁ (ξ - ψ₂) + ω₂ (ξ + ψ₁) + ω₃ (ξ + 1))
+# \end{aligned}
+# ```
+# where $ω = (ω_1, ω_2, ω_3)$ is the angular velocity in a body-fixed frame,
+# $ψ = (ψ_1, ψ_2, ψ_3)$ are the Rodrigues parameters, and
+# $ξ = ψ₁^2 + ψ₂^2 + ψ₃^2$.
 
-    aux = ψ₁^2 + ψ₂^2 + ψ₃^2
+vars_idx = Dict(:states => 1:6, :controls => 7:9)
+
+@taylorize function AttitudeControl!(dx, x, p, t)
+    ω₁, ω₂, ω₃, ψ₁, ψ₂, ψ₃, u₀, u₁, u₂ = x
+
+    ξ = ψ₁^2 + ψ₂^2 + ψ₃^2
 
     dx[1] = 0.25 * (u₀ + ω₂ * ω₃)
     dx[2] = 0.5 * (u₁ - 3 * ω₁ * ω₃)
     dx[3] = u₂ + 2 * ω₁ * ω₂
-    dx[4] = 0.5 * (  ω₂ * (aux - ψ₃)
-                   + ω₃ * (aux + ψ₂)
-                   + ω₁ * (aux + 1))
-    dx[5] = 0.5 * (  ω₁ * (aux + ψ₃)
-                   + ω₃ * (aux - ψ₁)
-                   + ω₂ * (aux + 1))
-    dx[6] = 0.5 * (  ω₁ * (aux - ψ₂)
-                   + ω₂ * (aux + ψ₁)
-                   + ω₃ * (aux + 1))
+    dx[4] = 0.5 * (  ω₂ * (ξ - ψ₃)
+                   + ω₃ * (ξ + ψ₂)
+                   + ω₁ * (ξ + 1))
+    dx[5] = 0.5 * (  ω₁ * (ξ + ψ₃)
+                   + ω₃ * (ξ - ψ₁)
+                   + ω₂ * (ξ + 1))
+    dx[6] = 0.5 * (  ω₁ * (ξ - ψ₂)
+                   + ω₂ * (ξ + ψ₁)
+                   + ω₃ * (ξ + 1))
     dx[7] = zero(u₀)
     dx[8] = zero(u₁)
     dx[9] = zero(u₂)
     return dx
-end
+end;
+
+# We are given a neural-network controller with 3 hidden layers of 64 neurons
+# each and sigmoid activations. The controller has 6 inputs (the state
+# variables) and 3 outputs ($u_0, u_1, u_2$).
+
+path = @modelpath("AttitudeControl", "AttitudeControl_controller.polar")
+controller = read_POLAR(path);
+
+# The control period is 0.1 time units.
+
+period = 0.1;
 
 # ## Specification
 
-# the simpler network format is more efficient to parse than the ONNX format
-controller = read_POLAR(@modelpath("AttitudeControl", "CLF_controller_layer_num_3"));
-# import ONNX
-# path = @modelpath("AttitudeControl", "attitude_control_3_64_torch.onnx")
-# controller_onnx = read_ONNX(path; input_dimension=6);
-# @assert controller_onnx == controller
+# The uncertain initial condition is:
 
-## The initial states according to the specification are:
 X₀ = Hyperrectangle(low=[-0.45, -0.55, 0.65, -0.75, 0.85, -0.65],
                     high=[-0.44, -0.54, 0.66, -0.74, 0.86, -0.64])
 U₀ = ZeroSet(3);
 
-## The system has 6 state variables and 3 control variables:
-vars_idx = Dict(:states=>1:6, :controls=>7:9)
-ivp = @ivp(x' = AttitudeControl!(x), dim: 9, x(0) ∈ X₀ × U₀)
-period = 0.1;  # control period
+# The control problem is:
 
+ivp = @ivp(x' = AttitudeControl!(x), dim: 9, x(0) ∈ X₀ × U₀)
 prob = ControlledPlant(ivp, controller, vars_idx, period);
 
-## Safety specification
-T = 3.0  # time horizon
+# The safety specification is that a set of unsafe states should not be reached
+# within 3 time units. A sufficient condition for guaranteed verification is to
+# overapproximate the result with hyperrectangles.
+
 unsafe_states = cartesian_product(
     Hyperrectangle(low=[-0.2, -0.5, 0,   -0.7, 0.7, -0.4],
                    high=[0,   -0.4, 0.2, -0.6, 0.8, -0.2]),
     Universe(3))
-predicate = X -> isdisjoint(overapproximate(X, Hyperrectangle), unsafe_states);
 
-# ## Results
+predicate(sol) = isdisjoint(overapproximate(sol, Hyperrectangle), unsafe_states);
 
-# To integrate the ODE, we use the Taylor-model-based algorithm:
+T = 3.0
+T_warmup = 2 * period;  # shorter time horizon for warm-up run
+
+# ## Analysis
+
+# To enclose the continuous dynamics, we use a Taylor-model-based algorithm:
+
 alg = TMJets(abstol=1e-6, orderT=6, orderQ=1);
 
 # To propagate sets through the neural network, we use the `DeepZ` algorithm:
-alg_nn = DeepZ()
 
+alg_nn = DeepZ();
 
-function benchmark(; silent::Bool=false)
-    ## We solve the controlled system:
-    silent || println("flowpipe construction")
-    res_sol = @timed solve(prob, T=T, alg_nn=alg_nn, alg=alg)
-    sol = res_sol.value
-    silent || print_timed(res_sol)
+# The verification benchmark is given below:
 
-    ## Next we check the property for an overapproximated flowpipe:
-    silent || println("property checking")
-    res_pred = @timed predicate(sol)
-    silent || print_timed(res_pred)
-    if res_pred.value
-        silent || println("The property is satisfied.")
+function benchmark(; T=T, silent::Bool=false)
+    ## Solve the controlled system:
+    silent || println("Flowpipe construction:")
+    res = @timed solve(prob; T=T, alg_nn=alg_nn, alg=alg)
+    sol = res.value
+    silent || print_timed(res)
+
+    ## Check the property:
+    silent || println("Property checking:")
+    res = @timed predicate(sol)
+    silent || print_timed(res)
+    if res.value
+        silent || println("  The property is satisfied.")
     else
-        silent || println("The property may be violated.")
+        silent || println("  The property may be violated.")
     end
+
     return sol
-end
+end;
 
-benchmark(silent=true)  # warm-up
-res = @timed benchmark()  # benchmark
+# Run the verification benchmark and compute some simulations:
+
+benchmark(T=T_warmup, silent=true)  # warm-up
+res = @timed benchmark(T=T)  # benchmark
 sol = res.value
-println("total analysis time")
-print_timed(res);
+println("Total analysis time:")
+print_timed(res)
 
-# We also compute some simulations:
-
-import DifferentialEquations
-
-println("simulation")
-res = @timed simulate(prob, T=T, trajectories=10, include_vertices=false)
+println("Simulation:")
+res = @timed simulate(prob; T=T, trajectories=10, include_vertices=false)
 sim = res.value
 print_timed(res);
 
-# Finally we plot the results:
+# ## Results
 
-using Plots
-import DisplayAs
+# Script to plot the results:
 
-function plot_helper(fig, vars; show_simulation::Bool=true)
-    plot!(fig, sol, vars=vars, color=:yellow, lab="")
-    unsafe_states_projected = project(unsafe_states, vars)
-    plot!(fig, unsafe_states_projected, color=:red, alpha=:0.2,
-          lab="unsafe states", leg=:topleft)
+function plot_helper!(fig, vars; show_simulation::Bool=true)
+    plot!(fig, project(unsafe_states, vars); color=:red, alpha=:0.2,
+          lab="unsafe", leg=:topleft)
+    plot!(fig, sol; vars=vars, color=:yellow, lab="")
+    plot!(fig, project(X₀, vars); c=:cornflowerblue, alpha=1, lab="X₀")
     if show_simulation
         plot_simulation!(fig, sim; vars=vars, color=:black, lab="")
     end
     fig = DisplayAs.Text(DisplayAs.PNG(fig))
-end
+end;
+
+# Plot the results:
 
 vars = (1, 2)
 fig = plot(xlab="ω₁", ylab="ω₂")
-plot_helper(fig, vars)
-# savefig("AttitudeControl-x1-x2.png")
-
-#-
+fig = plot_helper!(fig, vars)
+## savefig("AttitudeControl-x1-x2.png")  # command to save the plot to a file
 
 end  #jl
 nothing  #jl
+
+# ## References
+
+# [^PPR]: Stephen Prajna, Pablo A. Parrilo, and Anders Rantzer (2004).
+#         *Nonlinear control synthesis by convex optimization*. In
+#         [IEEE Trans. Autom. Control](https://doi.org/10.1109/TAC.2003.823000).
