8738bfc5135b14c69989722ae33eec7878bddf63
schillic <git@christianschilling.net>
2024-02-09 20:46:02 +0100

    format docs scripts

--- docs/generate.jl
+++ docs/generate.jl
@@ -2,6 +2,7 @@ import Literate
 import ClosedLoopReachability: @modelpath
 
 MODELS = [
+          #
           "ACC",
           "Airplane",
           "AttitudeControl",
@@ -12,7 +13,8 @@ MODELS = [
           "TORA",
           "Unicycle",
           "VerticalCAS"
-         ]
+          #
+          ]
 
 source_dir = joinpath(@__DIR__, "..", "models")
 target_dir = joinpath(@__DIR__, "src", "models")
@@ -35,13 +37,14 @@ for model in MODELS
                 Literate.markdown(input, target_dir; postprocess=mdpost, credit=false)
             else
                 # for the local build, one needs to set `nbviewer_root_url`
-                Literate.markdown(input, target_dir; postprocess=mdpost, credit=false, nbviewer_root_url="..")
+                Literate.markdown(input, target_dir; postprocess=mdpost, credit=false,
+                                  nbviewer_root_url="..")
             end
 
             # notebooks are deactivated to speed up the generation
-#             Literate.notebook(input, target_dir_md; execute=true, credit=false)
-# if used, add the following to the top of the script files (where `MODELNAME` is the model name):
-#md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/models/MODELNAME.ipynb)
+            # Literate.notebook(input, target_dir_md; execute=true, credit=false)
+            # if used, add the following to the top of the script files (where `MODELNAME` is the model name):
+            #md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/models/MODELNAME.ipynb)
         elseif any(endswith.(file, [".png", ".jpg", ".gif"]))
             cp(joinpath(model_path, file), joinpath(target_dir, file); force=true)
         elseif any(endswith.(file, [".md", ".polar"]))
